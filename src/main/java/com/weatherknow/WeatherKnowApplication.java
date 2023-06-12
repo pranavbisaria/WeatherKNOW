86fab42b0dc1fbc8115a0779f5b2ab7861b05a8d
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-06-12 23:54:33 +0530

    Initial Commit

---
+++ src/main/java/com/weatherknow/WeatherKnowApplication.java
@@ -0,0 +1,13 @@
+package com.weatherknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherKnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherKnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weatherknow/WeatherKnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherKnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
