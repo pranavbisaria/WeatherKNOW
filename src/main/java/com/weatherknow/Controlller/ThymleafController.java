0e33c11883136fa99a70559cb34bd766b092c748
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-06-17 01:23:22 +0530

    Fixed UI Bugs

--- src/main/java/com/weatherknow/Controlller/ThymleafController.java
+++ src/main/java/com/weatherknow/Controlller/ThymleafController.java
@@ -2,14 +2,20 @@ package com.weatherknow.Controlller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ThymleafController {
-        @GetMapping("/unsubscribe/{email}")
-        public String showUnsubscribePage(@PathVariable("email") String email, Model model) {
-            model.addAttribute("email", email);
-            return "unsubscribe";
-        }
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/unsubscribe/{email}")
+    public String showUnsubscribePage(@PathVariable("email") String email, Model model) {
+        model.addAttribute("email", email);
+        return "unsubscribe";
+    }
 }
